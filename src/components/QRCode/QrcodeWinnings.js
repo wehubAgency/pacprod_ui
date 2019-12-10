26501f624c096a670be772bb364f0d06c0749679
Arcades2 <peret.etienne@gmail.com>
2019-12-10 21:39:02 +0100

    qrcode winnings

--- src/components/QRCode/QrcodeWinnings.js
+++ src/components/QRCode/QrcodeWinnings.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Modal, Button, Table } from 'antd';
 import { useSelector } from 'react-redux';
 import { Translate } from 'react-localize-redux';
+import { convertArrayToCSV } from 'convert-array-to-csv';
 import { useFetchData } from '../../hooks';
 import generateColumns from '../../services/generateColumns';
 
@@ -19,6 +20,30 @@ const QrcodeWinnings = ({ qrcode }) => {
     setWinnings(data);
   }, [data]);
 
+  const downloadCsv = () => {
+    const headers = [
+      <Translate id="user" />,
+      <Translate id="email" />,
+      <Translate id="prize" />,
+      <Translate id="date" />,
+      <Translate id="used" />,
+    ];
+    const data = winnings.map(w => [
+      `${w.user.firstname} ${w.user.lastname}`,
+      w.user.email,
+      w.prize.model.name,
+      w.date,
+      w.used ? <Translate id="yes" /> : <Translate id="no" />,
+    ]);
+
+    const csv = convertArrayToCSV(data, {
+      headers,
+      separator: ';',
+    });
+
+    console.log({ csv });
+  };
+
   const columns = generateColumns(componentConfig, 'winningComponent');
 
   return (
@@ -26,7 +51,17 @@ const QrcodeWinnings = ({ qrcode }) => {
       <Button type="primary" onClick={() => setVisible(true)}>
         <Translate id="qrcodeWinnings.showWinnings" />
       </Button>
-      <Modal visible={visible} width="80%">
+      <Modal
+        visible={visible}
+        width="80%"
+        onCancel={() => setVisible(false)}
+        onOk={() => setVisible(false)}
+      >
+        {winnings.length > 0 && (
+          <Button type="primary" onClick={downloadCsv}>
+            <Translate id="qrcodeWinnings.downloadCsv" />
+          </Button>
+        )}
         <Table
           dataSource={winnings}
           columns={columns}
